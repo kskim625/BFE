6af63d78a2b60a7e9726f83d50d7661774ff6bee
Kyung Sik Kim <83746849+kskim625@users.noreply.github.com>
2021-06-21 00:15:44 +0900

    update in search pages
    some differences in search pages are made

--- bfe_search.js
+++ bfe_search.js
@@ -1,3 +1,13 @@
 function search_goals(name){
+  let search = document.getElementByClassName('search_text');
+  let player = document.getElementByClassName('goalSearch');
 
+  window.open('search_goals.html')
+
+  for (let i = 0; i < goalSearch.length; i++){
+    playerName = player[i].getElementByClassName('player_name');
+    if (player === playerName){
+      player[i].style.color = 'white';
+    }
+  }
 }
