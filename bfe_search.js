3e375d370286244db6305e9ac9ff5f3041893292
Kyung Sik Kim <83746849+kskim625@users.noreply.github.com>
2021-06-21 22:01:16 +0900

    trying to make searching (through javascript) work
    trying to make searching (through javascript) work

--- bfe_search.js
+++ bfe_search.js
@@ -1,13 +1,30 @@
-function search_goals(name){
-  let search = document.getElementByClassName('search_text');
-  let player = document.getElementByClassName('goalSearch');
+function getName(){
+  const name = document.getElementByID('search_text').value;
+  alert(name);
+  return name;
+}
 
+function search_goals(){
+  let name = getName();
+  alert(name);
   window.open('search_goals.html')
+  document.getElementByClassName('text').innerText = name;
+  table = document.getElementByClassName('tables');
 
-  for (let i = 0; i < goalSearch.length; i++){
+
+  for (let i = 0; i < table.rows.length; i++){
+    /*
     playerName = player[i].getElementByClassName('player_name');
-    if (player === playerName){
-      player[i].style.color = 'white';
+    if (name === playerName){
+      player[i].style.color = 'yellow';
+      */
+    rIndex = this.rowindex;
+    var id = this.cells[2].textContent;
+    if (id === name){
+      alert(this.cells[0].textContent);
     }
+    else{
+      alert('none');
+    };
   }
 }
