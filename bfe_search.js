bac77bb663b8ae2acb1bfad79ed2cd875f15209b
Kyung Sik Kim <83746849+kskim625@users.noreply.github.com>
2021-07-09 22:50:54 +0900

    search page updated
    3 search pages are unified into 1, and few changes are made in the index file

--- bfe_search.js
+++ bfe_search.js
@@ -1,15 +1,59 @@
+function open_search_page(){
+  var new_page = window.open('search.html');
+}
+
 function search_goals(){
-  var current_page = window.location.href;
+
   var name = document.getElementById('search_text').value;
-
-  if (current_page !== "file:///C:/Users/kskim/Desktop/web/BFE/search_goals.html"){
-    var new_page = window.open('search_goals.html');
-  }
-
-  table = new_page.document.getElementById('tables');
+  table = document.getElementById('table_goals');
+  document.getElementById('table_goals').style.display="";
+  document.getElementById('table_assists').style.display="none";
+  document.getElementById('table_points').style.display="none";
 
   var i, playerName, player;
-  player = new_page.document.getElementsByClassName('goalSearch');
+  player = document.getElementsByClassName('goalSearch');
+
+  for (i = 0; i < table.rows.length; i++){
+    playerName = player[i].getElementsByClassName('player_name');
+    if (playerName[0].innerHTML.toLowerCase().indexOf(name) != -1){
+      player[i].bgColor = 'yellow';
+    } else {
+      player[i].bgColor = 'white';
+    }
+  }
+}
+
+function search_assists(){
+
+  var name = document.getElementById('search_text').value;
+  table = document.getElementById('table_assists');
+  document.getElementById('table_goals').style.display="none";
+  document.getElementById('table_assists').style.display="";
+  document.getElementById('table_points').style.display="none";
+
+  var i, playerName, player;
+  player = document.getElementsByClassName('assistSearch');
+
+  for (i = 0; i < table.rows.length; i++){
+    playerName = player[i].getElementsByClassName('player_name');
+    if (playerName[0].innerHTML.toLowerCase().indexOf(name) != -1){
+      player[i].bgColor = 'yellow';
+    } else {
+      player[i].bgColor = 'white';
+    }
+  }
+}
+
+function search_points(){
+
+  var name = document.getElementById('search_text').value;
+  table = document.getElementById('table_points');
+  document.getElementById('table_goals').style.display="none";
+  document.getElementById('table_assists').style.display="none";
+  document.getElementById('table_points').style.display="";
+
+  var i, playerName, player;
+  player = document.getElementsByClassName('pointSearch');
 
   for (i = 0; i < table.rows.length; i++){
     playerName = player[i].getElementsByClassName('player_name');
